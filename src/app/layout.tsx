808787f6b2e20563d4caf3479d06c40c8556a13f
rihan-a <a.rihan@live.com>
2025-02-06 12:36:27 +0100

    add Head to wrap the google symbols link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,10 +21,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
            <link
                     rel="stylesheet"
                     href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined"
                 />
+      </head>
       <body
         className={`${inter.variable} antialiased`}
       >
