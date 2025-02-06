2757049bcb8d7c9ab7be964cca6b034d9bdbaad3
rihan-a <a.rihan@live.com>
2025-02-06 17:23:38 +0100

    move child components into the Dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,7 @@
-import PromptForm from "../components/PromptForm";
-import Table from "../components/Table";
+import Dashboard from "@/components/Dashboard";
 
 export default function Home() {
   return (
-    <div className="h-screen flex items-center justify-center relative ">
-            <div className="absolute top-[12px] left-[12px]">
-                <PromptForm />
-            </div>
-
-            <div className="flex items-center justify-center">
-                <Table />
-            </div>
-        </div>
+   <Dashboard/>
   );
 }
