bf60ddf334bca9e6e2019f3f99c64318c8ef0951
rihan-a <a.rihan@live.com>
2025-02-06 13:14:27 +0100

    render the main component in the root

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+import PromptForm from "./components/PromptForm";
+import Table from "./components/Table";
+
 export default function Home() {
   return (
-    <div>
-      Hello World!!
-    </div>
+    <div className="h-screen flex items-center justify-center relative ">
+            <div className="absolute top-[12px] left-[12px]">
+                <PromptForm />
+            </div>
+
+            <div className="flex items-center justify-center">
+                <Table />
+            </div>
+        </div>
   );
 }
