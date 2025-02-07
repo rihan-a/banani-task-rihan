bf6b93187bebd9b6819e653aafecfa26d83b6c8e
rihan-a <a.rihan@live.com>
2025-02-07 16:53:58 +0100

    lift onDelete fucntion to parent

--- src/components/TableBody.tsx
+++ src/components/TableBody.tsx
@@ -3,13 +3,14 @@ import TableRow from "./TableRow";
 interface TableBodyProps {
     rows: any[];
     headers: string[];
+    onDelete: (row: any) => void;
 }
 
-export default function TableBody({ rows, headers }: TableBodyProps) {
+export default function TableBody({ rows, headers, onDelete }: TableBodyProps) {
     return (
         <tbody>
             {rows.map((row, index) => (
-                <TableRow key={index} row={row} headers={headers} />
+                <TableRow key={index} row={row} headers={headers} onDelete={onDelete} />
             ))}
         </tbody>
     );
