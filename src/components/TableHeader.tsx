9aa63652d1d213bd8358e382785672a2abfdda34
rihan-a <a.rihan@live.com>
2025-02-07 18:29:07 +0100

    update styling for small screens

--- src/components/TableHeader.tsx
+++ src/components/TableHeader.tsx
@@ -9,13 +9,13 @@ export default function TableHeader({ headers }: TableHeaderProps) {
                 {headers.map((header: string, index: number) => (
                     <th 
                         key={index} 
-                        className={`p-2 text-left text-sm ${index === 0 ? 'font-medium pl-[16px]' : 'font-normal'} text-gray-700 border-b bg-white ${index === 0 ? 'min-w-[200px]' : 'min-w-[96px]'}`}
+                        className={`p-2 text-left text-sm ${index === 0 ? 'font-medium pl-[16px]' : 'font-normal'} text-gray-700 border-b bg-white ${index === 0 ? '   md:min-w-[200px]' : 'md:min-w-[96px]'}`}
                     >
                         {header}
                     </th>
                 ))}
                 <th 
-                    className="p-1 text-left text-sm font-normal text-gray-700 border-b bg-white min-w-[48px]">
+                    className="p-1 text-left text-sm font-normal text-gray-700 border-b bg-white w-[20px] md:min-w-[48px]">
                     Actions
                 </th>
             </tr>
