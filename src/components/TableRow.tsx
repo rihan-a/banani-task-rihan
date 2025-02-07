6192a60f08dca9c14ea049e6e3ca28dfdcbb15a7
rihan-a <a.rihan@live.com>
2025-02-07 16:09:05 +0100

    minor style udpates

--- src/components/TableRow.tsx
+++ src/components/TableRow.tsx
@@ -15,7 +15,7 @@ export default function TableRow({ row, headers }: TableRowProps) {
     return (
         <tr className="hover:bg-gray-50 transition border-b bg-white h-[56px]">
             {headers.map((header, colIndex) => (
-                <td key={colIndex} className={`p-3 text-sm text-gray-700 ${colIndex === 0 ? 'font-medium' : 'font-normal'}`}>
+                <td key={colIndex} className={`p-2 text-sm text-gray-700 ${colIndex === 0 ? 'font-medium pl-[16px]' : 'font-normal'}`}>
                     {isIconItem(row[header]) ? (
                         <div className="flex items-center">
                             <span className="material-symbols-outlined text-gray-600 mr-2">
@@ -28,13 +28,13 @@ export default function TableRow({ row, headers }: TableRowProps) {
                     )}
                 </td>
             ))}
-            <td className="p-3">
+            <td className="min-w-[48px] pr-[16px]">
                 <div className="flex">
                     <span className="material-symbols-outlined">
                         bookmark
                     </span>
-                    <span className="material-symbols-outlined pl-[10px]">
-                        more_horiz
+                    <span className="material-symbols-outlined pl-[8px]">
+                        delete
                     </span>
                 </div>
             </td>
