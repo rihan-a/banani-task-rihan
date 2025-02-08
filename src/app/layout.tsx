1e789d0c092d3f2550b2039128b0e4a6d805213c
rihan-a <a.rihan@live.com>
2025-02-08 19:36:48 +0100

    [bug] update google icons link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,6 @@ const inter = Inter({
   subsets: ["latin"],
 });
 
-
-
 export const metadata: Metadata = {
   title: "Banani Task Rihan",
   description: "Generate table from prompt using AI",
@@ -22,14 +20,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-           <link
-                    rel="stylesheet"
-                    href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined&display=optional"
-                />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+        />
       </head>
-      <body
-        className={`${inter.variable} antialiased`}
-      >
+      <body className={`${inter.variable} antialiased`}>
         {children}
       </body>
     </html>
