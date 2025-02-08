1ba31856982ed117c3fe2d0df52b44db665b3f70
rihan-a <a.rihan@live.com>
2025-02-08 14:12:14 +0100

    add condition to render the header

--- src/components/TableHeader.tsx
+++ src/components/TableHeader.tsx
@@ -1,24 +1,27 @@
 interface TableHeaderProps {
     headers: string[];
+    hasData: boolean;
 }
 
-export default function TableHeader({ headers }: TableHeaderProps) {
+export default function TableHeader({ headers, hasData }: TableHeaderProps) {
     return (
         <thead className="bg-white sticky top-0">
             <tr className="h-[56px]">
                 {headers.map((header: string, index: number) => (
                     <th 
                         key={index} 
-                        className={`p-2 text-left text-sm ${index === 0 ? 'font-medium pl-[16px]' : 'font-normal'} text-gray-700 border-b bg-white ${index === 0 ? '   md:min-w-[200px]' : 'md:min-w-[96px]'}`}
+                        className={`p-2 text-left text-sm ${index === 0 ? 'font-medium pl-[16px]' : 'font-normal'} text-gray-700 border-b bg-white ${index === 0 ? 'md:min-w-[200px]' : 'md:min-w-[96px]'}`}
                     >
                         {header}
                     </th>
                 ))}
-                <th 
-                    className="p-1 text-left text-sm font-normal text-gray-700 border-b bg-white w-[20px] md:min-w-[48px]">
-                    Actions
-                </th>
+                {hasData && (
+                    <th 
+                        className="p-1 text-left text-sm font-normal text-gray-700 border-b bg-white w-[20px] md:min-w-[48px]">
+                        Actions
+                    </th>
+                )}
             </tr>
         </thead>
     );
-} 
+}
