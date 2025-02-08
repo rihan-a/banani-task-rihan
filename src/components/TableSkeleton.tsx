58dec72248d9ed26278dc3783f0afe3775a4a855
rihan-a <a.rihan@live.com>
2025-02-08 14:19:24 +0100

    udpate skeleton width

--- src/components/TableSkeleton.tsx
+++ src/components/TableSkeleton.tsx
@@ -5,18 +5,18 @@ import { motion } from "framer-motion";
 export default function TableSkeleton() {
     return (
         <motion.div
-            className="animate-pulse w-[96vw] sm:w-[200px] md:w-[640px] lg:w-[640px] h-[160px] bg-gray-200 rounded-lg p-4 flex flex-col gap-2"
+            className="animate-pulse w-[96vw] sm:w-[200px] md:w-[640px] lg:w-[740px] h-[160px] bg-gray-200 rounded-lg p-4 flex flex-col gap-2"
             initial={{ opacity: 0, y: -10 }}
             animate={{ opacity: 1, y: 0 }}
             exit={{ opacity: 0, y: -10 }}
             transition={{ duration: 0.5 }}
         >
             {/* mock header */}
-            <div className="h-6 bg-gray-300 rounded w-full"></div>
+            <div className="h-7 bg-gray-300 rounded w-full"></div>
             {/* mock rows */}
-            <div className="h-6 bg-gray-300 rounded w-full"></div>
-            <div className="h-6 bg-gray-300 rounded w-full"></div>
-            <div className="h-6 bg-gray-300 rounded w-full"></div>
+            <div className="h-7 bg-gray-300 rounded w-full"></div>
+            <div className="h-7 bg-gray-300 rounded w-full"></div>
+            <div className="h-7 bg-gray-300 rounded w-full"></div>
         </motion.div>
     );
 }
